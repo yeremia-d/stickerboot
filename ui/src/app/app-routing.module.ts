867300b64fd3268b459b3b7589ee13c0b9576e32
yeremia-d <yeremia.djaja@gmail.com>
2019-10-28 19:21:19 -0400

    started homepage

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from './homepage/homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent, pathMatch: 'full'}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CommonComponentsModule} from './common-components/common-components.module';
+import {HomepageModule} from './homepage/homepage.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {CommonComponentsModule} from './common-components/common-components.modu
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonComponentsModule
+    CommonComponentsModule,
+    HomepageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
