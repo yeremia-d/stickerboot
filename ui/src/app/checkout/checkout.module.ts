8dc54b628f4c78ae4388e822fe1744b9aeefdaf2
yeremia-d <yeremia.djaja@gmail.com>
2019-10-28 23:59:55 -0400

    started checkout ui

--- ui/src/app/checkout/checkout.module.ts
+++ ui/src/app/checkout/checkout.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
+import {MatStepperModule} from '@angular/material';
 
 
 
 @NgModule({
   declarations: [CheckoutPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatStepperModule
   ]
 })
 export class CheckoutModule { }
