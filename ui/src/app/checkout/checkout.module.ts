90b6a8f730609a41dfd28a2fcb4c61928448bea7
yeremia-d <yeremia.djaja@gmail.com>
2019-10-29 16:39:00 -0400

    more work on checkout ui

--- ui/src/app/checkout/checkout.module.ts
+++ ui/src/app/checkout/checkout.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
-import {MatStepperModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule, MatIconModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatStepperModule
+} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
+import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 
 
 @NgModule({
-  declarations: [CheckoutPageComponent],
+  declarations: [CheckoutPageComponent, CheckoutFormComponent, OrderSummaryComponent],
   imports: [
     CommonModule,
-    MatStepperModule
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatIconModule
   ]
 })
 export class CheckoutModule { }
