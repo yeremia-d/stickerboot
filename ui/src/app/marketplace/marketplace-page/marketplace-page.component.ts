6a82559ce52c1d4d54169ad197db04a7765cb692
yeremia-d <yeremia.djaja@gmail.com>
2019-10-29 21:53:19 -0400

    Marketplace ui implementation

--- ui/src/app/marketplace/marketplace-page/marketplace-page.component.ts
+++ ui/src/app/marketplace/marketplace-page/marketplace-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Sticker} from '../../models/sticker.model';
 
 @Component({
   selector: 'app-marketplace-page',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketplacePageComponent implements OnInit {
 
-  constructor() { }
+  stickers: Array<Sticker>;
+  numItems: number;
+  pageSize: number;
+  pageSizeOptions: Array<number>;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.stickers = [
+      {name: 'sticker1', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]},
+      {name: 'sticker2', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]},
+      {name: 'sticker3', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]},
+      {name: 'sticker4', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]},
+      {name: 'sticker5', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]},
+      {name: 'sticker6', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]},
+      {name: 'sticker7', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]},
+      {name: 'sticker8', sizes: [{height: 1, width: 1}], basePrice: 5.99, materials: [{materialName: 'matte vinyl'}]}
+    ];
+
+    this.pageSize = 10;
+    this.numItems = 100;
+    this.pageSizeOptions = [5, 10, 15, 20, 25];
+
+  }
 }
