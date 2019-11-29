189e731e42c658b4457871e5d86bc88d66ac14df
yeremia-d <yeremia.djaja@gmail.com>
2019-11-29 13:25:18 -0500

    Updated CatalogServerApplication

--- applications/catalog-server/src/main/java/com/acn/stickerboot/catalog/CatalogServerApplication.java
+++ applications/catalog-server/src/main/java/com/acn/stickerboot/catalog/CatalogServerApplication.java
@@ -1,5 +1,12 @@
 package com.acn.stickerboot.catalog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CatalogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServerApplication.class, args);
+    }
 }
