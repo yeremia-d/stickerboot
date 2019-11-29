d08b1d0475f88a39e29bcc5d8384a092fa91d452
yeremia-d <yeremia.djaja@gmail.com>
2019-11-29 14:36:10 -0500

    Cleaned up some gradle config, updated CatalogService

--- components/catalog/src/main/java/com/acn/stickerboot/catalog/CatalogService.java
+++ components/catalog/src/main/java/com/acn/stickerboot/catalog/CatalogService.java
@@ -1,4 +1,17 @@
 package com.acn.stickerboot.catalog;
 
+import com.acn.stickerboot.catalog.data.Item;
+import org.springframework.data.domain.Page;
+
 public interface CatalogService {
+
+    Item getItemById(Long id);
+
+    Page<Item> listItems();
+
+    Item addItem();
+
+    Item updateItem();
+
+    void deleteById(Long id);
 }
